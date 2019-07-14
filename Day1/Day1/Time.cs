eec022b343166d78efa7b2d7db18dc72fc8c23c6
tshaul <itamirshaul@gmail.com>
2019-07-14 13:19:00 +0300

    Added day 1 solution

---
+++ Day1/Day1/Time.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Time
+    {
+        private static Time INSTANCE;
+        private static object key = new object();
+        private Time()
+        {
+
+        }
+
+        public static Time GetInstance()
+        {
+            if (INSTANCE != null)
+            {
+                return INSTANCE;
+            }
+            lock (key)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Time();
+                }
+            }
+            return INSTANCE;
+        }
+
+        public void PrintTime()
+        {
+            Console.WriteLine("The time is:" + DateTime.Now.ToString());
+        }
+    }
+}
