52bf7d4ebe4481e58fbc42ace34fb01a1fde87cc
tshaul <itamirshaul@gmail.com>
2019-07-17 16:42:24 +0300

    aaaa

---
+++ Day4/Day4/FlyWeight/FlyWeightFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4.FlyWeight
+{
+    public class FlyWeightFactory<T>
+    {
+        Dictionary<char, VeryHeavyObject> repository = new Dictionary<char, VeryHeavyObject>();
+
+        VeryHeavyObject GetHeavyObjects(char typeOfObject)
+        {
+            if(this.repository.TryGetValue(typeOfObject, out VeryHeavyObject result))
+            {
+                return result;
+            }
+
+            VeryHeavyObject newObject = null;
+
+            switch (typeOfObject)
+            {
+                case 'A':
+                    newObject = new VeryHeavyObject();
+                    break;
+                case 'N':
+                    newObject = new VeryHeavyObject();
+                    break;
+                case 'O':
+                    newObject = new VeryHeavyObject();
+                    break;
+
+            }
+
+            repository[typeOfObject] = newObject;
+
+            return newObject;
+        }
+    }
+}
---
+++ Day4/Day4/FlyWeight/VeryHeavyObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4.FlyWeight
+{
+    public class VeryHeavyObject
+    {
+        static Random r = new Random();
+        public const int MAX_SIZE = 1_000_000;
+        int[] items;
+
+        public VeryHeavyObject()
+        {
+            this.items = new int[MAX_SIZE];
+
+            for (int i = 0; i < MAX_SIZE; i++)
+            {
+                items[i] = r.Next(1000);
+            }
+        }
+
+        public T this [int index]
+        {
+            get
+            {
+                return items[index];
+            }
+        }
+    }
+}
